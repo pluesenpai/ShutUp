37e13cab242d47829e371a173a764477c347e448
Paolo Cortis <paolo.cortis@gmail.com>
2012-06-03 17:16:05 +0200

    If ringing, when phone is flipped it goes to vibrate mode.

--- src/org/slackcnt/android/shutup/ShutUpActivity.java
+++ src/org/slackcnt/android/shutup/ShutUpActivity.java
@@ -1,32 +1,50 @@
 package org.slackcnt.android.shutup;
 
+import org.slackcnt.android.shutup.ShutUpService.LocalBinder;
+
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 
 public class ShutUpActivity extends Activity
 {
-	// private final ServiceConnection serviceConnection = new
-	// ServiceConnection() {
-	// @Override
-	// public void onServiceDisconnected(ComponentName arg0)
-	// {
-	// }
-	//
-	// @Override
-	// public void onServiceConnected(ComponentName name, IBinder service)
-	// {
-	// }
-	// };
+	private static ShutUpService shutUpService;
+
+	private final ServiceConnection serviceConnection = new ServiceConnection()
+	{
+		@Override
+		public void onServiceDisconnected(ComponentName arg0)
+		{
+			shutUpService = null;
+		}
+
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service)
+		{
+			shutUpService = ((LocalBinder) service).getService();
+		}
+	};
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+	}
+
+	@Override
+	protected void onResume()
+	{
+		super.onResume();
 
 		Intent intent = new Intent(this, ShutUpService.class);
-		bindService(intent, null, Context.BIND_AUTO_CREATE);
+		if(shutUpService == null) {
+			startService(intent);
+		}
+		bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
 	}
 }
--- src/org/slackcnt/android/shutup/ShutUpService.java
+++ src/org/slackcnt/android/shutup/ShutUpService.java
@@ -3,7 +3,10 @@ package org.slackcnt.android.shutup;
 import java.util.List;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -11,12 +14,13 @@ import android.hardware.SensorManager;
 import android.media.AudioManager;
 import android.os.Binder;
 import android.os.IBinder;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 public class ShutUpService extends Service implements SensorEventListener
 {
-	// TODO: capture phone events, enable ShutUp when ringing and restore old
-	// values of ringer and vibrate at end of ringing
+	private static final String TAG = ShutUpService.class.getName();
 
 	private final IBinder mBinder = new LocalBinder();
 
@@ -30,15 +34,74 @@ public class ShutUpService extends Service implements SensorEventListener
 
 	private float last_z;
 
+	private int ringerModeBefore = -1;
+
+	private final BroadcastReceiver telephonyBroadcastReceiver = new BroadcastReceiver()
+	{
+		@Override
+		public void onReceive(Context context, Intent intent)
+		{
+			PhoneStateListener phoneStateListener = new PhoneStateListener()
+			{
+				@Override
+				public void onCallStateChanged(int state, String incomingNumber)
+				{
+					Log.i(TAG, "Call state changed");
+
+					switch(state) {
+						case TelephonyManager.CALL_STATE_OFFHOOK:
+						case TelephonyManager.CALL_STATE_IDLE:
+							Log.i(TAG, "Call closed or answered");
+							disableSensorListener();
+							break;
+						case TelephonyManager.CALL_STATE_RINGING:
+							Log.i(TAG, "Incoming call");
+							enableSensorListener();
+							break;
+					}
+				}
+			};
+
+			TelephonyManager telephony = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+			telephony.listen(phoneStateListener,
+					PhoneStateListener.LISTEN_CALL_STATE);
+		}
+	};
+
 	@Override
 	public void onCreate()
 	{
 		super.onCreate();
-		Log.i(ShutUpService.class.getName(), "onCreate");
+		Log.i(TAG, "onCreate");
 
 		audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId)
+	{
+		Log.i(TAG, "onStartCommand");
+
+		registerReceiver(telephonyBroadcastReceiver, new IntentFilter(
+				TelephonyManager.ACTION_PHONE_STATE_CHANGED));
+
+		return START_NOT_STICKY;
+	}
+
+	private void enableSensorListener()
+	{
+		Log.i(TAG, "enableSensorListener");
 		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
+		if(audioManager.getRingerMode() != AudioManager.RINGER_MODE_NORMAL) {
+			// do nothing if ringer is in mode vibrate or silent
+			ringerModeBefore = -1;
+			return;
+		}
+
+		Log.i(TAG, "enabling listeners");
+
+		ringerModeBefore = audioManager.getRingerMode();
 		List<Sensor> sensorList = sensorManager
 				.getSensorList(Sensor.TYPE_ACCELEROMETER);
 		if(sensorList.size() > 0) {
@@ -46,14 +109,56 @@ public class ShutUpService extends Service implements SensorEventListener
 			sensorManager.registerListener(this, accelerometer,
 					SensorManager.SENSOR_DELAY_NORMAL);
 		} else {
-			Log.i(ShutUpService.class.getName(), "No accelerometer");
+			Log.i(TAG, "No accelerometer");
 		}
+	}
+
+	private void disableSensorListener()
+	{
+		Log.i(TAG, "disableSensorListener");
+		if(sensorManager == null) {
+			return;
+		}
+
+		if(ringerModeBefore > -1) {
+			Log.i(TAG, "restoring ringer");
+			audioManager.setRingerMode(ringerModeBefore);
+		}
+
+		sensorManager.unregisterListener(this);
+		sensorManager = null;
 	}
 
 	@Override
 	public IBinder onBind(Intent intent)
 	{
 		return mBinder;
+	}
+
+	public void onAccuracyChanged(Sensor sensor, int accuracy)
+	{
+		Log.i(TAG, "onAccuracyChanged");
+	}
+
+	public void onSensorChanged(SensorEvent event)
+	{
+		if(event.sensor.getType() != Sensor.TYPE_ACCELEROMETER) {
+			return;
+		}
+
+		Log.i(TAG, "onSensorChanged");
+		long curTime = System.currentTimeMillis();
+
+		if((curTime - lastUpdate) > 100) {
+			lastUpdate = curTime;
+
+			float z = event.values[2];
+			if(last_z >= 0 && z < 0) {
+				Log.i(TAG, "phone flipped face down");
+				audioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+			}
+			last_z = z;
+		}
 	}
 
 	public class LocalBinder extends Binder
@@ -63,29 +168,4 @@ public class ShutUpService extends Service implements SensorEventListener
 			return ShutUpService.this;
 		}
 	}
-
-	public void onAccuracyChanged(Sensor sensor, int accuracy)
-	{
-		Log.i(ShutUpService.class.getName(), "onAccuracyChanged");
-	}
-
-	public void onSensorChanged(SensorEvent event)
-	{
-		Log.i(ShutUpService.class.getName(), "onSensorChanged");
-		long curTime = System.currentTimeMillis();
-
-		// only allow one update every 100ms.
-		if((curTime - lastUpdate) > 100) {
-			lastUpdate = curTime;
-
-			// TODO: save old state of volume and vibrate
-			float z = event.values[2];
-			if(last_z >= 0 && z < 0) {
-				audioManager
-						.setStreamMute(AudioManager.STREAM_VOICE_CALL, true);
-			}
-			last_z = z;
-		}
-	}
-
 }
