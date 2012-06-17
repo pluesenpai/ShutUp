5a320724527afd6d3790368db880aa467f24cc62
Paolo Cortis <paolo.cortis@gmail.com>
2012-06-17 16:27:04 +0200

    holo theme on android >= 3.0 standard theme on previous versions

--- gen/org/slackcnt/android/shutup/R.java
+++ gen/org/slackcnt/android/shutup/R.java
@@ -14,9 +14,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int btnStartStop=0x7f050001;
-        public static final int startAtBoot=0x7f050002;
-        public static final int txtStatus=0x7f050000;
+        public static final int btnStartStop=0x7f060001;
+        public static final int startAtBoot=0x7f060002;
+        public static final int txtStatus=0x7f060000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -29,5 +29,8 @@ public final class R {
         public static final int start_at_boot=0x7f040006;
         public static final int stop=0x7f040005;
         public static final int stopped=0x7f040003;
+    }
+    public static final class style {
+        public static final int ShutUpTheme=0x7f050000;
     }
 }
